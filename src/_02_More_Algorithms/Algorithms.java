8947c2219bc96f4c370f7b93637c40556853412c
Chloe Luwa <chloeluwa@yahoo.com>
2018-08-17 15:50:38 -0700

    Algorithms, SearchingAndSortingAlgorithms
    Algorithms, SearchingAndSortingAlgorithms

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -58,20 +58,48 @@ public class Algorithms {
 		return answer;
 	}
 	
-	public static double sortScores(List<Double> scores) {
+	public static List<Double> sortScores(List<Double> scores) {
 		for(int i = 0; i < scores.size(); i++) {
 			for(int j = i+1; j<scores.size(); j++) {
 				if(scores.get(i) > scores.get(j)) {
+					
 					double temp = scores.get(i);
-					scores.get(i) = scores.get(j);
-					scores.get(j) = temp;
+					scores.set(i, scores.get(j));
+					scores.set(j, temp);
+				}
+			}
+		}
+		return scores;
+	}
+	
+	public static List<String> sortDNA(List<String> DNA){
+		for(int i = 0; i<DNA.size(); i++) {
+			for(int j = i+1; j<DNA.size(); j++) {
+				if(DNA.get(i).length() > DNA.get(j).length()) {
+					String temp = DNA.get(i);
+					DNA.set(i, DNA.get(j));
+					DNA.set(j, temp);
+				}
+			}
+		}
+		return DNA;
+		}
+	
+	public static List<String> sortWords(List<String> words){
+		for(int i = 0; i<words.size(); i++) {
+			for(int j = i+1; j<words.size(); j++) {
+				if(words.get(i).compareTo(words.get(j)) > 0) {
+					String t = words.get(i);
+					words.set(i, words.get(j));
+					words.set(j,t);
 				}
 				
 			}
-			
 		}
 		
 		
+		return words;
 	}
 	
 }
+
